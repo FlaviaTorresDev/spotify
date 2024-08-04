dc6b88eb601cac84d9dcf17655d73241d1c82bd3
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-03 21:57:12 -0300

    interface de autorização

---
+++ spotify/src/main/java/flavia/dev/spotify/cliente/AuthSpotifyClient.java
@@ -0,0 +1,17 @@
+package flavia.dev.spotify.cliente;
+
+import java.awt.PageAttributes.MediaType;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "AuthSpotifyClient",
+        url = "https://accounts.spotify.com"
+)
+public interface AuthSpotifyClient {
+
+    @PostMapping(value = "/api/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    LoginResponse login(@RequestBody LoginRequest loginRequest);
+}
