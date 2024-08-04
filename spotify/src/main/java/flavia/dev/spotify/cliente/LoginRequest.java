ff55a983c3c97b2471a5f2dbed59bbfdcf692237
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-03 22:05:32 -0300

    request e response add

---
+++ spotify/src/main/java/flavia/dev/spotify/cliente/LoginRequest.java
@@ -0,0 +1,48 @@
+package flavia.dev.spotify.cliente;
+
+import feign.form.FormProperty;
+
+public class LoginRequest {
+	
+	   @FormProperty("grant_type")
+	    private String grantType;
+	    @FormProperty("client_id")
+	    private String clientId;
+	    @FormProperty("client_secret")
+	    private String clientSecret;
+
+	    public LoginRequest() {
+	    }
+
+	    public LoginRequest(String grantType, String clientId, String clientSecret) {
+	        this.grantType = grantType;
+	        this.clientId = clientId;
+	        this.clientSecret = clientSecret;
+	    }
+
+	    public String getGrantType() {
+	        return grantType;
+	    }
+
+	    public void setGrantType(String grantType) {
+	        this.grantType = grantType;
+	    }
+
+	    public String getClientId() {
+	        return clientId;
+	    }
+
+	    public void setClientId(String clientId) {
+	        this.clientId = clientId;
+	    }
+
+	    public String getClientSecret() {
+	        return clientSecret;
+	    }
+
+	    public void setClientSecret(String clientSecret) {
+	        this.clientSecret = clientSecret;
+	    }
+	}
+
+}
---
+++ spotify/src/main/java/flavia/dev/spotify/cliente/LoginResponse.java
@@ -0,0 +1,26 @@
+package flavia.dev.spotify.cliente;
+
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LoginResponse {
+
+    private String accessToken;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
